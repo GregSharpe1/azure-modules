8678d84bad8191cdeba64b9687074dd530425fbb
Greg Sharpe <me@gregsharpe.co.uk>
2019-01-10 10:35:31 +0000

    formatted to powershell instead of powershell workflow

--- AzureStartShutDownWithTags.ps1
+++ AzureStartShutDownWithTags.ps1
@@ -1,65 +1,63 @@
-workflow PowerVMs {
-  # Stop VMs based on the tags parameter
-  Param (
-      [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
-      [String]
-      $VMTag="Development",
-      [Parameter(Mandatory=$true)][ValidateSet("Start","Stop")]
-      [String]
-      $Action
-    )
+# Stop VMs based on the tags parameter
+Param (
+    [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()]
+    [String]
+    $VMTag="Development",
+    [Parameter(Mandatory=$true)][ValidateSet("Start","Stop")]
+    [String]
+    $Action
+  )
 
-  try
-  {
-      # Run as accounts must be setup within the Azure account setup
-      $connectionName = "AzureRunAsConnection"
+try
+{
+    # Run as accounts must be setup within the Azure account setup
+    $connectionName = "AzureRunAsConnection"
 
-      # Get the connection "AzureRunAsConnection"
-      $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+    # Get the connection "AzureRunAsConnection"
+    $servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
 
-      Write-Output "LOG: Logging in to Azure..."
-      Add-AzureRmAccount `
-          -ServicePrincipal `
-          -TenantId $servicePrincipalConnection.TenantId `
-          -ApplicationId $servicePrincipalConnection.ApplicationId `
-          -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
-  }
-  catch {
-      if (!$servicePrincipalConnection)
-      {
-          $ErrorMessage = "ERROR: Connection $connectionName not found."
-          throw $ErrorMessage
-      } else{
-          Write-Error -Message $_.Exception
-          throw $_.Exception
-      }
-  }
-
-  # Collect all VMs with Names similar to the value passed
-  $VMs = Get-AzureRmVm
-  # TODO: Parse the VM tag inputted! Both environment set within Azure and the parameter being passed in.
-  # $ParsedVMTag = $VMTag.
-
-  # Check if we have found any VMs before continuing
-  if($VMs)
-  {
-    # Loop over each VM object returned
-    foreach -parallel ($vm in $VMs) {
-      # If the VM's Environment tag equals the parameter set
-      if ($_.Tags['Environment'] -eq $VMTag) {
-        # If the action parameter is stop, stop the current VM
-        if ($Action -eq "Stop") {
-          Write-Output "LOG: Stopping $_.Name"
-          Stop-AzureRmVm -ResourceGroupName $_.ResourceGroupName -Name $_.Name -Force -ErrorAction SilentlyContinue
-        } else {
-          Write-Output "LOG: Starting $_.Name"
-          Start-AzureRmVm -ResourceGroupName $_.ResourceGroupName -Name $_.Name -Force -ErrorAction SilentlyContinue
-        }
-      } else {
-        Write-Output "LOG: $_ does not have tags containing $VMTag therefore not stopping..."
-      }
+    Write-Output "LOG: Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "ERROR: Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
     }
-  } else {
-    Write-Output "LOG: No VMs were found in your subscription."
+}
+
+# Collect all VMs with Names similar to the value passed
+$VMs = Get-AzureRmVm
+# TODO: Parse the VM tag inputted! Both environment set within Azure and the parameter being passed in.
+# $ParsedVMTag = $VMTag.
+
+# Check if we have found any VMs before continuing
+if($VMs)
+{
+  # Loop over each VM object returned
+  foreach ($vm in $VMs) {
+    # If the VM's Environment tag equals the parameter set
+    if ($_.Tags['Environment'] -eq $VMTag) {
+      # If the action parameter is stop, stop the current VM
+      if ($Action -eq "Stop") {
+        Write-Output "LOG: Stopping $_.Name"
+        Stop-AzureRmVm -ResourceGroupName $_.ResourceGroupName -Name $_.Name -Force -ErrorAction SilentlyContinue
+      } else {
+        Write-Output "LOG: Starting $_.Name"
+        Start-AzureRmVm -ResourceGroupName $_.ResourceGroupName -Name $_.Name -Force -ErrorAction SilentlyContinue
+      }
+    } else {
+      Write-Output "LOG: $_ does not have tags containing $VMTag therefore not stopping..."
+    }
   }
+} else {
+  Write-Output "LOG: No VMs were found in your subscription."
 }
